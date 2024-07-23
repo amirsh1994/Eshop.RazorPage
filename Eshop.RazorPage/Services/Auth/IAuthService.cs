76b7d49dac9763f97111fdb6f7672deece501598
Amir Sharabi <shahrabi527@gmail.com>
2024-07-23 03:18:52 -0700

    99/01 Create Controllers Services

--- Eshop.RazorPage/Services/Auth/IAuthService.cs
+++ Eshop.RazorPage/Services/Auth/IAuthService.cs
@@ -8,7 +8,7 @@ public interface IAuthService
 
 public class AuthService(HttpClient client,CancellationToken cancellationToken) : IAuthService
 {
-    public async Task Register()
+    public async Task RAdd()
     {
         await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, "as"), cancellationToken);
     }
