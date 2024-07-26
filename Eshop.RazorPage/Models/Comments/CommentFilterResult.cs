50ffb3117e3d78e9e25a63dcd2dda3ef22c91643
Amir Sharabi <shahrabi527@gmail.com>
2024-07-26 07:42:54 -0700

    99-6 complete commentService

---
+++ Eshop.RazorPage/Models/Comments/CommentFilterResult.cs
@@ -0,0 +1,60 @@
+﻿namespace Eshop.RazorPage.Models.Comments;
+
+public class CommentFilterResult:BaseFilter<CommentDto,CommentsFilterParam>
+{
+
+}
+
+public class CommentDto:BaseDto
+{
+    public int ProductId { get; set; }
+
+    public string ProductTitle { get; set; }
+
+    public string Text { get; set; }
+
+    public int Status { get; set; }
+
+    public int UserId { get; set; }
+
+    public string UserFullName { get; set; }
+
+}
+
+public class CommentsFilterParam:BaseFilterParam
+{
+    public int UserId { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public int Status { get; set; }
+
+}
+
+public class CreateCommentCommand
+{
+    public string Text { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ProductId { get; set; }
+}
+public class EditCommentCommand
+{
+    public long CommentId { get; set; }
+
+    public string Text { get; set; }
+
+    public int UserId { get; set; }
+
+}
+public enum CommentStatus
+{
+
+    Rejected,
+    Pending,
+    Accepted
+
+}
--- Eshop.RazorPage/Services/Comments/ICommentService.cs
+++ Eshop.RazorPage/Services/Comments/ICommentService.cs
@@ -1,11 +1,61 @@
-﻿namespace Eshop.RazorPage.Services.Comments;
+﻿using Eshop.RazorPage.Models;
+using Eshop.RazorPage.Models.Comments;
+
+namespace Eshop.RazorPage.Services.Comments;
 
 public interface ICommentService
 {
-    
+    Task<CommentFilterResult?> GetCommentsByFilter(CommentsFilterParam filter);
+
+    Task<CommentDto?> GetCommentById(long commentId);
+
+    Task<ApiResult?> CreateComment(CreateCommentCommand command);
+
+    Task<ApiResult?> EditComment(EditCommentCommand command);
+
+    Task<ApiResult?> ChangeStatus(long commentId, CommentStatus status);
+
+
+
+
+
+
 }
 
-public class CommentService:ICommentService
+public class CommentService(HttpClient client) : ICommentService
 {
+    public async Task<CommentFilterResult?> GetCommentsByFilter(CommentsFilterParam filter)
+    {
+        var result = await client.GetFromJsonAsync<ApiResult<CommentFilterResult>>($"comment?UserId={filter.UserId}&StartDate={filter.StartDate}EndDate={filter.EndDate}&Status={filter.Status}&PageId={filter.PageId}&Take={filter.Take}");
+        var response = result?.Data;
+        return response;
+    }
 
+    public async Task<CommentDto?> GetCommentById(long commentId)
+    {
+        var result = await client.GetFromJsonAsync<ApiResult<CommentDto>>($"comment/{commentId}");
+        return result?.Data;
+    }
+
+    public async Task<ApiResult?> CreateComment(CreateCommentCommand command)
+    {
+        var result = await client.PostAsJsonAsync("comment", command);
+        var response = await result.Content.ReadFromJsonAsync<ApiResult>();
+        return response;
+    }
+
+    public async Task<ApiResult?> EditComment(EditCommentCommand command)
+    {
+        var result = await client.PutAsJsonAsync("comment", command);
+        var response = await result.Content.ReadFromJsonAsync<ApiResult>();
+        return response;
+
+    }
+
+    public async Task<ApiResult?> ChangeStatus(long commentId, CommentStatus status)
+    {
+        var result = await client.PutAsJsonAsync("comment/changeStatus", new { commentId, status });
+        var response = await result.Content.ReadFromJsonAsync<ApiResult>();
+        return response;
+    }
 }
