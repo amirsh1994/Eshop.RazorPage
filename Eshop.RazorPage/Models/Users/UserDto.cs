7f3f2d0b956600a48a3b13fe447321516561365f
Amir Sharabi <shahrabi527@gmail.com>
2024-07-28 02:57:26 -0700

    Add User Service

---
+++ Eshop.RazorPage/Models/Users/UserDto.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+
+namespace Eshop.RazorPage.Models.Users;
+
+public class UserDto:BaseDto
+{
+    public string Name { get; set; }
+
+    public string Family { get; set; }
+
+    public string AvatarName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public List<UserRoleDto> UserRoles { get; set; } = new();
+}
+
+public class UserRoleDto:BaseDto
+{
+    public long RoleId { get; set; }
+
+    public string RoleTitle { get; set; }
+
+}
+public enum Gender
+{
+    None,
+    Male,
+    Female
+}
+
+public class UserFilterResult:BaseFilter<UserFilterData,UserFilterParams>
+{
+
+}
+
+public class UserFilterData:BaseDto
+{
+    public string Name { get; set; }
+
+    public string Family { get; set; }
+
+    public string AvatarName { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public Gender Gender { get; set; }
+}
+
+public class UserFilterParams:BaseFilterParam
+{
+    public long? Id { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+}
--- Eshop.RazorPage/Services/Users/IUserService.cs
+++ Eshop.RazorPage/Services/Users/IUserService.cs
@@ -1,13 +1,99 @@
-﻿namespace Eshop.RazorPage.Services.Users;
+﻿using Eshop.RazorPage.Infrastructure;
+using Eshop.RazorPage.Models;
+using Eshop.RazorPage.Models.Users;
+using Eshop.RazorPage.Models.Users.Commands;
+
+namespace Eshop.RazorPage.Services.Users;
 
 public interface IUserService
 {
-    
+    Task<ApiResult?> CreateUser(CreateUserCommand command);
+
+    Task<ApiResult?> EditUser(EditUserCommand command);
+
+    Task<ApiResult?> EditUserCurrent(EditUserCommand command);
+
+    Task<UserFilterResult?> GetUsersByFilter(UserFilterParams filterParams);
+
+    Task<UserDto?> GetUserById(long userId);
+
+    Task<UserDto?> GetUserByPhoneNumber(string phoneNumber);
+
+    Task<UserDto?> GetCurrentUser();
+
+
+
+
+
 }
 
 
 
-public class UserService:IUserService
+public class UserService(HttpClient client):IUserService
 {
+    private const string ModuleName = "User";
+    public async Task<ApiResult?> CreateUser(CreateUserCommand command)
+    {
+        var result = await client.PostAsJsonAsync(ModuleName, command);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
 
+    public async Task<ApiResult?> EditUser(EditUserCommand command)
+    {
+        var formData = new MultipartFormDataContent();
+        formData.Add(new StringContent(command.Email), "Email");
+        formData.Add(new StringContent(command.Family), "Family");
+        formData.Add(new StringContent(command.Name), "Name");
+        formData.Add(new StringContent(command.Password), "Password");
+        formData.Add(new StringContent(command.PhoneNumber), "PhoneNumber");
+        formData.Add(new StringContent(command.Gender.ToString()), "Gender");
+        formData.Add(new StringContent(command.UserId.ToString()), "UserId");
+        formData.Add(new StreamContent(command.Avatar.OpenReadStream()), "Avatar", command.Avatar.FileName);
+        var result = await client.PostAsync(ModuleName, formData);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<ApiResult?> EditUserCurrent(EditUserCommand command)
+    {
+        var formData = new MultipartFormDataContent();
+        formData.Add(new StringContent(command.Email), "Email");
+        formData.Add(new StringContent(command.Family), "Family");
+        formData.Add(new StringContent(command.Name), "Name");
+        formData.Add(new StringContent(command.Password), "Password");
+        formData.Add(new StringContent(command.PhoneNumber), "PhoneNumber");
+        formData.Add(new StringContent(command.Gender.ToString()), "Gender");
+        formData.Add(new StringContent(command.UserId.ToString()), "UserId");
+        if(command.Avatar!=null)
+            formData.Add(new StreamContent(command.Avatar.OpenReadStream()), "Avatar", command.Avatar.FileName);
+
+        var result = await client.PostAsync($"{ModuleName}/current", formData);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+
+    }
+
+    public async Task<UserFilterResult?> GetUsersByFilter(UserFilterParams filterParams)
+    {
+        var url = $"{filterParams.GenerateBaseFilterUrl(ModuleName)}" +
+                  $"&Email={filterParams.Email}&PhoneNumber={filterParams.PhoneNumber}&Id={filterParams.Id}";
+        var result = await client.GetFromJsonAsync<ApiResult<UserFilterResult>>(url);
+        return result?.Data;
+    }
+
+    public async Task<UserDto?> GetUserById(long userId)
+    {
+        var result = await client.GetFromJsonAsync<ApiResult<UserDto>>($"{ModuleName}/{userId}");
+        return result?.Data;
+    }
+
+    public async Task<UserDto?> GetUserByPhoneNumber(string phoneNumber)
+    {
+        var result = await client.GetFromJsonAsync<ApiResult<UserDto>>($"{ModuleName}/{phoneNumber}");
+        return result?.Data;
+    }
+
+    public async Task<UserDto?> GetCurrentUser()
+    {
+        var result = await client.GetFromJsonAsync<ApiResult<UserDto>>($"{ModuleName}/current");
+        return result?.Data;
+    }
 }
