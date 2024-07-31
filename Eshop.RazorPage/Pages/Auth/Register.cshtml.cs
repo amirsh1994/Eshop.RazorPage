07f8467b64c1352b2ebe945209770b4c434dfd67
Amir Sharabi <shahrabi527@gmail.com>
2024-07-31 00:47:22 -0700

    02-Auth 02-Register

--- Eshop.RazorPage/Pages/Auth/Register.cshtml.cs
+++ Eshop.RazorPage/Pages/Auth/Register.cshtml.cs
@@ -1,12 +1,52 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Eshop.RazorPage.Models.Auth;
+using Eshop.RazorPage.Services.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Eshop.RazorPage.Pages.Auth
+namespace Eshop.RazorPage.Pages.Auth;
+
+[BindProperties]
+[ValidateAntiForgeryToken]
+public class RegisterModel(IAuthService service) : PageModel
 {
-    public class RegisterModel : PageModel
+    [Display(Name = "شماره تلفن")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [MaxLength(11, ErrorMessage = "حدقل 11 کاراکتر")]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "کلمه عبور")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    public string Password { get; set; }
+
+    [Display(Name = "تکرار کلمه عبور")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [Compare(nameof(Password), ErrorMessage = "کلمه‌های عبور یکسان نیستند")]
+    public string ConfirmedPassword { get; set; }
+
+    public void OnGet()
     {
-        public void OnGet()
+
+    }
+
+
+    public async Task<IActionResult> OnPost()
+    {
+      var result= await service.Register(new RegisterCommand
         {
+            PhoneNumber = PhoneNumber,
+            Password = Password,
+            ConfirmPassword = ConfirmedPassword
+        });
+        if (result?.IsSuccess==false)
+        {
+            ModelState.AddModelError(nameof(PhoneNumber),result.MetaData.Message);
+            return Page();
         }
+        return RedirectToPage("Login");
+
     }
 }
+
+
