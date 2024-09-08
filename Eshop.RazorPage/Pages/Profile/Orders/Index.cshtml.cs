1725f6e3797b243a6c2679a390c0a5beea71e8bb
Amir Sharabi <shahrabi527@gmail.com>
2024-09-08 00:46:09 -0700

    complete order index cshtml

--- Eshop.RazorPage/Pages/Profile/Orders/Index.cshtml.cs
+++ Eshop.RazorPage/Pages/Profile/Orders/Index.cshtml.cs
@@ -1,11 +1,16 @@
+using Eshop.RazorPage.Models.Orders;
+using Eshop.RazorPage.Services.Orders;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Eshop.RazorPage.Pages.Profile.Orders;
 
-    public class IndexModel : PageModel
+    public class IndexModel(IOrderService orderService) : PageModel
     {
-        public async Task OnGet()
+        public OrderFilterResult FilterResult { get; set; }
+
+        public async Task OnGet(int take=10,int pageId=1,OrderStatus ? status=null)
         {
+            FilterResult = await orderService.GetUserOrdersByFilter(10, pageId, status);
         }
     }
 
